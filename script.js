774bb4f56bb4a7eae1efb999727185b3f5a1b06a
Joridua2108 <jaoriolsduarte.juegos@gmail.com>
2026-02-15 21:48:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function actualizarReloj() {
+    const ahora = new Date();
+    
+    const horas = String(ahora.getHours()).padStart(2, '0');
+    const minutos = String(ahora.getMinutes()).padStart(2, '0');
+    const segundos = String(ahora.getSeconds()).padStart(2, '0');
+    
+    document.getElementById('reloj').textContent = `${horas}:${minutos}:${segundos}`;
+}
+
+
+setInterval(actualizarReloj, 1000);
+actualizarReloj();
